14d55ff627fa4fc00c2f35dbce62248e06e3bd37
owais <syedowaisali@users.noreply.github.com>
2022-07-27 14:54:21 +0500

    improvements

--- app/src/main/java/pk/sufiishq/app/activities/MainActivity.kt
+++ app/src/main/java/pk/sufiishq/app/activities/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import pk.sufiishq.app.data.repository.KalamRepository
-import pk.sufiishq.app.db.SufiIshqDatabase
 import pk.sufiishq.app.services.AudioPlayerService
 import pk.sufiishq.app.ui.screen.MainView
 import pk.sufiishq.app.ui.theme.SufiIshqTheme
@@ -22,13 +21,10 @@ import pk.sufiishq.app.viewmodels.PlayerViewModel
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), ServiceConnection {
 
     @Inject
     lateinit var kalamRepository: KalamRepository
-
-    @Inject
-    lateinit var db: SufiIshqDatabase
 
     private val playerViewModel: PlayerViewModel by viewModels()
     private val playerIntent by lazy { Intent(this, AudioPlayerService::class.java) }
@@ -43,14 +39,8 @@ class MainActivity : ComponentActivity() {
         }
 
         init {
-            bindService(playerIntent, serviceConnection, Context.BIND_AUTO_CREATE)
+            bindService(playerIntent, this, Context.BIND_AUTO_CREATE)
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        playerViewModel.disposeDownload()
-        unbindService(serviceConnection)
     }
 
     private fun init(initiated: () -> Unit) {
@@ -68,26 +58,29 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private val serviceConnection = object : ServiceConnection {
-        override fun onServiceConnected(componentName: ComponentName?, service: IBinder?) {
-            val binder = service as AudioPlayerService.AudioPlayerBinder
-            val playerController = binder.getService()
+    override fun onDestroy() {
+        super.onDestroy()
+        playerViewModel.disposeDownload()
+        unbindService(this)
+    }
 
-            if (playerController.getActiveTrack() == null) {
-                kalamRepository.getDefaultKalam().observeOnce(this@MainActivity) { kalam ->
-                    playerController.setActiveTrack(kalam)
-                    playerViewModel.setPlayerService(playerController)
-                    playerController.setPlayerListener(playerViewModel)
-                }
-            } else {
+    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+        val binder = service as AudioPlayerService.AudioPlayerBinder
+        val playerController = binder.getService()
+
+        if (playerController.getActiveTrack() == null) {
+            kalamRepository.getDefaultKalam().observeOnce(this@MainActivity) { kalam ->
+                playerController.setActiveTrack(kalam)
                 playerViewModel.setPlayerService(playerController)
                 playerController.setPlayerListener(playerViewModel)
             }
-
+        } else {
+            playerViewModel.setPlayerService(playerController)
+            playerController.setPlayerListener(playerViewModel)
         }
+    }
 
-        override fun onServiceDisconnected(componentName: ComponentName?) {
-            playerViewModel.setPlayerService(null)
-        }
+    override fun onServiceDisconnected(name: ComponentName?) {
+        playerViewModel.setPlayerService(null)
     }
 }
