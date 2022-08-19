d84d76c9127eb3cd7a9e2a840495d03e61a047f8
syedowaisali <dp.owaisali@gmail.com>
2022-08-19 13:35:12 +0500

    update readme and remove unused imports from MainActivity

--- app/src/main/java/pk/sufiishq/app/activities/MainActivity.kt
+++ app/src/main/java/pk/sufiishq/app/activities/MainActivity.kt
@@ -1,13 +1,10 @@
 package pk.sufiishq.app.activities
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import pk.sufiishq.app.core.event.dispatcher.EventDispatcher
-import pk.sufiishq.app.core.event.events.PlayerEvents
-import pk.sufiishq.app.helpers.TrackListType
 import pk.sufiishq.app.ui.screen.MainView
 import pk.sufiishq.app.ui.theme.SufiIshqTheme
 
