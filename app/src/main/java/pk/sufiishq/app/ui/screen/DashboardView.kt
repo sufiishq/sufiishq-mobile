2b2cfca9d596b52b4eb259d0accbf958d7291768
syedowaisali <dp.owaisali@gmail.com>
2023-01-27 19:38:36 +0500

    remove test code

--- app/src/main/java/pk/sufiishq/app/ui/screen/DashboardView.kt
+++ app/src/main/java/pk/sufiishq/app/ui/screen/DashboardView.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,10 +34,8 @@ import pk.sufiishq.aurora.layout.SIBox
 import pk.sufiishq.aurora.layout.SIColumn
 import pk.sufiishq.aurora.layout.SIConstraintLayout
 import pk.sufiishq.aurora.layout.SIRow
-import pk.sufiishq.aurora.models.ColorPalette
 import pk.sufiishq.aurora.theme.AuroraDark
 import pk.sufiishq.aurora.theme.AuroraLight
-import pk.sufiishq.aurora.widgets.SIColorPaletteView
 
 @Composable
 fun DashboardView(
@@ -179,14 +176,6 @@ fun DashboardView(
                     }
                 }
             }
-        }
-
-        SIRow(modifier = Modifier.align(Alignment.TopStart)) {
-
-            SIColorPaletteView(colorPalette = ColorPalette.Clover, onClick = {})
-            SIColorPaletteView(colorPalette = ColorPalette.Blackberry, onClick = {})
-            SIColorPaletteView(colorPalette = ColorPalette.Grape, onClick = {})
-            SIColorPaletteView(colorPalette = ColorPalette.Blueberry, onClick = {})
         }
     }
 }
