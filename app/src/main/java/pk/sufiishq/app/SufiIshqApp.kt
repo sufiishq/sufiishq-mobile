33ec06d7650c462199fbe6a82fd5205387241a61
syedowaisali <dp.owaisali@gmail.com>
2023-02-16 16:33:26 +0500

    :hammer: :za: update version code and other updates

--- app/src/main/java/pk/sufiishq/app/SufiIshqApp.kt
+++ app/src/main/java/pk/sufiishq/app/SufiIshqApp.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 import java.util.concurrent.TimeUnit
@@ -12,6 +15,7 @@ import pk.sufiishq.app.core.storage.KeyValueStorage
 import pk.sufiishq.app.di.qualifier.SecureSharedPreferences
 import pk.sufiishq.app.worker.CacheRemoveWorker
 import timber.log.Timber
+import kotlin.math.log
 
 @HiltAndroidApp
 class SufiIshqApp : Application() {
@@ -24,6 +28,7 @@ class SufiIshqApp : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        logDebugAppCheckToken()
 
         // enable crashlytics only in release build
         FirebaseCrashlytics.getInstance()
@@ -42,6 +47,14 @@ class SufiIshqApp : Application() {
             CacheRemoveWorker.TAG,
             ExistingPeriodicWorkPolicy.KEEP,
             cacheRemoveWorkRequest
+        )
+    }
+
+    private fun logDebugAppCheckToken() {
+        FirebaseApp.initializeApp(this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            DebugAppCheckProviderFactory.getInstance()
         )
     }
 
